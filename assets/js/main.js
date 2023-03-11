543bbb223181503e228b424d9609dd33bb8cf141
Arthaud <arthaudp33@gmail.com>
2023-03-11 13:37:24 +0100

    feat: remove bugged asscroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,54 +2,30 @@ gsap.registerPlugin(ScrollTrigger);
 
 const isTouch = 'ontouchstart' in document.documentElement
 
-const asscroll = new ASScroll({
-    disableRaf: true
-});
-
-gsap.ticker.add(asscroll.update)
-
-ScrollTrigger.defaults({
-    scroller: asscroll.containerElement
-})
-
-ScrollTrigger.scrollerProxy(asscroll.containerElement, {
-    scrollTop(value) {
-        return arguments.length ? asscroll.currentPos = value : asscroll.currentPos;
-    },
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
-    }
-});
-
-asscroll.on("update", ScrollTrigger.update);
-ScrollTrigger.addEventListener("refresh", asscroll.resize);
-
 window.addEventListener("load", () => {
-    const totalScroll = asscroll.containerElement.scrollHeight - innerHeight;
+    // const totalScroll = asscroll.containerElement.scrollHeight - innerHeight;
 
-    gsap.to(".peach", {
-        scrollTrigger: {
-            pin: true,
-            pinType: isTouch ? 'fixed' : 'transform',
-            end: '200%',
-            scrub: 0.2,
-            trigger: ".peaches"
-        },
-        y: (i, target) => -totalScroll * target.dataset.speed,
-        ease: "none"
-    });
+    // gsap.to(".peach", {
+    //     scrollTrigger: {
+    //         pin: true,
+    //         pinType: isTouch ? 'fixed' : 'transform',
+    //         end: '200%',
+    //         scrub: 0.2,
+    //         trigger: ".peaches"
+    //     },
+    //     y: (i, target) => -totalScroll * target.dataset.speed,
+    //     ease: "none"
+    // });
 
-    gsap.from(".gif img", {
+    gsap.from(".test img", {
         scrollTrigger: {
             pin: true,
             pinType: isTouch ? 'fixed' : 'transform',
             scrub: true,
-            trigger: ".gif"
+            trigger: ".test"
         },
         scale: 0.2,
         autoAlpha: 0,
         ease: "sine.out"
     });
-
-    asscroll.enable();
 });
