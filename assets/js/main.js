d0e0b5d5f69a1cb160df88aae3f98b795e6fc7b2
Arthaud <arthaudp33@gmail.com>
2023-03-11 20:54:54 +0100

    feat: improve index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
+const smoother = ScrollSmoother.create({
     smooth: 2,               // how long (in seconds) it takes to "catch up" to the native scroll position
     effects: true,           // looks for data-speed and data-lag attributes on elements
-    smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
 
 const isTouch = 'ontouchstart' in document.documentElement
@@ -18,7 +16,6 @@ window.addEventListener("load", () => {
             pinType: isTouch ? 'fixed' : 'transform',
             scrub: 1,
             trigger: ".intro",
-            end: "200%",
         },
         y: (i, target) => -totalScroll * target.dataset.s,
         ease: "none"
