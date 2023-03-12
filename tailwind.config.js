6884e54223d15dbfdb8ff9954fe5c47bde8d84ba
Arthaud <arthaudp33@gmail.com>
2023-03-12 17:34:23 +0100

    style: 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: [
-    "./themes/**/layouts/**/*.html",
-    "./content/**/layouts/**/*.html",
-    "./layouts/**/*.html",
-    "./content/**/*.html"
-  ],
-  theme: {
-    extend: {
-      aspectRatio: {
-        portrait: '2 / 3',
-        landscape: '3 / 2',
-      }
+    content: [
+        "./themes/**/layouts/**/*.html",
+        "./content/**/layouts/**/*.html",
+        "./layouts/**/*.html",
+        "./content/**/*.html"
+    ],
+    theme: {
+        extend: {
+            aspectRatio: {
+                portrait: '2 / 3',
+                landscape: '3 / 2',
+            }
+        },
+        fontFamily: {
+            'sans': ['Geomanist', ...defaultTheme.fontFamily.sans],
+            'display': ['Manuscribe'],
+        },
+        colors: {
+            primary: 'white',
+            secondary: '#111111',
+            transparent: 'transparent'
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+            },
+        },
     },
-    fontFamily: {
-      'sans': ['Geomanist', ...defaultTheme.fontFamily.sans],
-      'display': ['Manuscribe'],
-    },
-    colors: {
-      primary: 'white',
-      secondary: '#111111',
-    },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-      },
-    },
-  },
-  plugins: [
-  ]
+    plugins: []
 }
